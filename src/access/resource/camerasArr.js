52a8f9542e957f5ff9b5ba9ab714ec38aabc5b14
Serg <serg27makar@gmail.com>
2020-06-21 10:19:32 +0300

    resource

---
+++ src/access/resource/camerasArr.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+const camerasArr = [
+    {
+        cameraName: "Camera 1",
+        cameraUrl: "./image/images0.jpg",
+        peopleState: {
+            standing: 10,
+            walking: 20,
+            running: 3,
+            sitting: 400,
+            bentOver: 50,
+            moving: 6,
+            resting: 10,
+            motionless: 700,
+        }
+    },
+    {
+        cameraName: "Camera 2",
+        cameraUrl: "./image/images1.jpg",
+        peopleState: {
+            standing: 11,
+            walking: 20,
+            running: 3,
+            sitting: 400,
+            bentOver: 50,
+            moving: 6,
+            resting: 10,
+            motionless: 700,
+        }
+    },
+    {
+        cameraName: "Camera 3",
+        cameraUrl: "./image/images2.jpg",
+        peopleState: {
+            standing: 12,
+            walking: 20,
+            running: 3,
+            sitting: 400,
+            bentOver: 50,
+            moving: 6,
+            resting: 10,
+            motionless: 700,
+        }
+    },
+    {
+        cameraName: "Camera 4",
+        cameraUrl: "./image/images3.jpg",
+        peopleState: {
+            standing: 13,
+            walking: 20,
+            // running: 3,
+            // sitting: 400,
+            // bentOver: 50,
+            moving: 6,
+            resting: 10,
+            motionless: 700,
+        }
+    },
+    {
+        cameraName: "Camera 5",
+        cameraUrl: "./image/images4.jpg",
+        peopleState: {
+            // standing: 14,
+            // walking: 20,
+            // running: 3,
+            // sitting: 400,
+            // bentOver: 50,
+            // moving: 6,
+            // resting: 10,
+            motionless: 700,
+        }
+    },
+    {
+        cameraName: "Camera 6",
+        cameraUrl: "./image/images0.jpg",
+        peopleState: {
+            standing: 15,
+            walking: 20,
+            // running: 3,
+            sitting: 400,
+            bentOver: 50,
+            moving: 6,
+            resting: 10,
+            // motionless: 700,
+        }
+    },
+    {
+        cameraName: "Camera 7",
+        cameraUrl: "./image/images1.jpg",
+        peopleState: {
+            standing: 16,
+            walking: 20,
+            running: 3,
+            sitting: 400,
+            bentOver: 50,
+            moving: 6,
+            resting: 10,
+            motionless: 700,
+        }
+    },
+];
+
+export default camerasArr;
